738a6fccbfd6c42209b7f2dbee1ec2d2b78fdee2
Ricardo Robles <rroblesgo@gmail.com>
2023-09-19 20:32:20 +0200

    Registro de Usuarios - Segundo

---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const TOKEN_SECRET = 'MY_TOKEN_SECRET';
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,13 +1,25 @@
+import bcryptjs from 'bcryptjs';
+import { createAccessToken } from '../libs/jwt.js';
 import User from '../models/user.model.js';
 
 export const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    const newUser = new User({ username, email, password });
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
     const userSaved = await newUser.save();
-    res.send({ message: 'User created', userSaved });
+    const user = {
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    };
+    const token = await createAccessToken({ id: userSaved._id });
+    res.cookie('access_token', token);
+    res.status(201).json({ message: 'User created', user, token });
   } catch (error) {
-    res.send({ message: 'Not created', error: error.message });
+    res.status(500).json({ message: 'Not created', error: error.message });
   }
 };
 
