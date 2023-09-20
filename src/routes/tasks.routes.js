64b75d3fd1e59b6fca189a049899caec4999b449
Ricardo Robles <rroblesgo@gmail.com>
2023-09-20 19:51:36 +0200

    Validación de datos Backend con zod

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -7,11 +7,18 @@ import {
   updateTask,
 } from '../controllers/tasks.controller.js';
 import { authRequired } from '../middlewares/validateToken.js';
+import { validateSchema } from '../middlewares/validator.middleware.js';
+import { createTaskSchema } from '../schemas/task.schema.js';
 
 const router = Router();
 
 router.get('/tasks', authRequired, getTasks);
-router.post('/tasks', authRequired, createTask);
+router.post(
+  '/tasks',
+  authRequired,
+  validateSchema(createTaskSchema),
+  createTask
+);
 router.get('/tasks/:id', authRequired, getTask);
 router.put('/tasks/:id', authRequired, updateTask);
 router.delete('/tasks/:id', authRequired, deleteTask);
